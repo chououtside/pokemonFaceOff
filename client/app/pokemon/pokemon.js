f764d78747fb54b350d1da66ccec2844f81a5d4e
chououtside <chouming3@gmail.com>
2016-02-16 11:54:54 -0800

    added images on pokemon page

--- client/app/pokemon/pokemon.js
+++ client/app/pokemon/pokemon.js
@@ -1,45 +1,35 @@
 angular.module('pokemon', [])
     .controller('pokemonController', function($scope, Pokemon) {
-        $scope.data = {
-            test: 'test'
-                // pokemon: [{
-                //        "name": "bulbasaur",
-                //        "url": "http://www.pokeapi.co/api/v2/pokemon/1/"
-                //    }, {
-                //        "name": "ivysaur",
-                //        "url": "http://www.pokeapi.co/api/v2/pokemon/2/"
-                //    }, {
-                //        "name": "venusaur",
-                //        "url": "http://www.pokeapi.co/api/v2/pokemon/3/"
-                //    }]
+        $scope.data = {         
         };
-
-        $scope.test = Pokemon.status;
-
 
         var initializePokemon = function() {
             Pokemon.getAll(function(data) {
                 $scope.$apply(function() {
-                    $scope.data.pokemon = data.slice(0,30);
+                    $scope.data.pokemon = data.slice(0, 30);
                 });
+             
+                for (var i = 0; i < $scope.data.pokemon.length; i++){
+                    $scope.data.pokemon[i].active = false;
+                }
 
-                console.log('client data is ', data);
-                console.log('scope data is ', $scope.data.pokemon);
                 console.log('new scope data is ', $scope.data.pokemon);
+
             });
 
         };
-        // var initializePokemon = function() {
-        //     Pokemon.getAllPromise()
-        //         .then(function(data) {
-        //             $scope.data.pokemon = data;
-        //         })
-        //         .catch(function(error) {
-        //             console.error(error);
-        //         });
+
+        $scope.toggle = function(index){
+            $scope.data.pokemon[index].active = !$scope.data.pokemon[index].active;
+
+        };
+
+        // $scope.save = function(userId) {
+        //     Pokemon.save();
+
         // };
 
-        console.log('before initalize Pokemon in model');
+
         initializePokemon();
 
     });
